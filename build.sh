4efc2cb5b4198f22fde0d6fc6c85ad3b08474e29
Diego Rocha <dhsrocha@gmail.com>
2017-11-30 22:33:28 -0200

    Pequenos consertos no build.sh

--- build.sh
+++ build.sh
@@ -23,50 +23,51 @@ echo;echo -e '\e[1;34m :: Processo construção da aplicação iniciado. ::\e[0m
   # Para que 'go get' seja usado de dentro da imagem docker
   if [ ! -f "src/$DEP" ]; then
 
-    echo;echo -e '\e[1;33m :: Baixando e construindo golang/dep. ::\e[0m'
+    echo;echo -e '\e[1;33m :: Baixando e construindo golang/dep. ::\e[0m';echo
     go get -u $DEP
-    go install $DEP
+    go build $DEP
 
   fi
   
   # Considerar a existência de um projeto vazio ou que
   # precisa ter o gerenciamento de dependência implementado
-  if [ -f "src/$APP/Gopkg.toml" ]; then
+  if [ ! -f "src/$APP/Gopkg.toml" ]; then
 
-    echo;echo -e '\e[1;33m :: Gopkg.toml não encontrado. Inicializando gerenciamento de dependências. ::\e[0m'
+    echo;echo -e '\e[33m :: Gopkg.toml não encontrado. Inicializando gerenciamento de dependências. ::\e[0m';echo
     bin/dep init src/$APP
   
   else
   
-    echo;echo -e '\e[1;32m :: Gopkg.toml encontrado. Instalando as dependências registradas. ::\e[0m'
+    echo;echo -e '\e[32m :: Gopkg.toml encontrado. Instalando as dependências registradas. ::\e[0m';echo
   
-  fi
+  fi	
   
+  # Instalar as dependências
   cd src/$APP
   ../../bin/dep ensure -v
-
   cd ../..
   
 } && {
 
+  # Teste
   echo;echo -e '\e[1;34m :: Test. ::\e[0m'
   go test -v $APP
 
 } && {
 
+  # Instalação
   echo;echo -e '\e[1;34m :: Install. ::\e[0m'
   go install -v $APP
 
 } && {
 
+  # Construção
   echo;echo -e '\e[1;34m :: Build. ::\e[0m'
   go build -v -o bin/$APP $APP
 
-} || {
+} || {  # Tratamento de exceção
 
-  echo;echo -e '\e[91m :: Falha na construção. ::\e[0m';echo
-  
-  exit 1
+  echo;echo -e '\e[91m :: Falha na construção. ::\e[0m' >&2
 
 }
 
